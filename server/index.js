f5386f65fb4b534f4945bebeed029f87c1f7a7a6
Prince Kumar <pk7411956419@gmail.com>
2024-08-13 15:59:12 +0530

    Completed!

--- server/index.js
+++ server/index.js
@@ -3,24 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-//!Express instance
 const app = express();
-//!Middlewares
+
 const corsOptions = {
   origin: ["http://localhost:5174", "http://localhost:5173"],
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
-// Access your API key as an environment variable (see "Set up your API key" above)
+
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-//!Generate content route
 app.post("/generate", async (req, res) => {
   const { prompt } = req.body;
   try {
-    // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
@@ -31,5 +28,4 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-//!Start the server
 app.listen(8080, console.log("Server is running"));
